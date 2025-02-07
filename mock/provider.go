e10a81bb38de48603ed013af901a263dcd72c90c
Peter Hellberg <peter@c7.se>
2025-02-07 14:46:23 +0100

    mock: Provider now implements llm.EmbedderClient

--- mock/provider.go
+++ mock/provider.go
@@ -6,12 +6,20 @@ import (
 	"github.com/peterhellberg/llm"
 )
 
-var _ llm.Provider = Provider{}
+var (
+	_ llm.Provider       = Provider{}
+	_ llm.EmbedderClient = Provider{}
+)
 
 type Provider struct {
 	GenerateContentFunc func(context.Context, []llm.Message, ...llm.ContentOption) (*llm.ContentResponse, error)
+	CreateEmbeddingFunc func(ctx context.Context, texts []string) ([][]float32, error)
 }
 
 func (p Provider) GenerateContent(ctx context.Context, messages []llm.Message, options ...llm.ContentOption) (*llm.ContentResponse, error) {
 	return p.GenerateContentFunc(ctx, messages, options...)
+}
+
+func (p Provider) CreateEmbedding(ctx context.Context, texts []string) ([][]float32, error) {
+	return p.CreateEmbeddingFunc(ctx, texts)
 }
